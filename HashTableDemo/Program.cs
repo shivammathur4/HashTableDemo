135763e0d276a32fd9c404466fb3901f3fe2ffb5
shivammathur4 <shivamm.4aug@gmail.com>
2021-05-17 12:47:27 +0530

    Shivam add Frequency

--- HashTableDemo/Program.cs
+++ HashTableDemo/Program.cs
@@ -10,15 +10,18 @@ namespace HashTableDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to hashtable demo");
+            Console.WriteLine("Hello, Welcome to HashTable!");
+            //creating an object of mapnode class
             MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
             hash.Add("0", "To");
             hash.Add("1", "be");
             hash.Add("2", "or");
             hash.Add("3", "not");
-            hash.Add("4", "to");
+            hash.Add("4", "To");
             hash.Add("5", "be");
-            hash.GetFrequency("or");
+            hash.GetFrequency("To");
+            string hash5 = hash.Get("5");
+            Console.WriteLine("5th index value: " + hash5);
             Console.ReadKey();
         }
     }
